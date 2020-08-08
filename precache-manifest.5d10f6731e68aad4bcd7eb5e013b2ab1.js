f00b5ab255aad5c9f025f1e18d3aca5d2b3af1f8
NaweedSufranj <sufranj.naweed@gmail.com>
2020-08-08 09:30:02 +0200

    deploy space-travlers !!! 🚀

---
+++ precache-manifest.5d10f6731e68aad4bcd7eb5e013b2ab1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6f2abdc6d0b01ebd7a",
+    "url": "/space-travelers/css/app.8f3c8d3a.css"
+  },
+  {
+    "revision": "1d3ea8e5d1aff469c3f69cedf93e7d70",
+    "url": "/space-travelers/img/space-travelers-rocket.1d3ea8e5.svg"
+  },
+  {
+    "revision": "3c98fbf9a05b83787e982bd90ab5c42e",
+    "url": "/space-travelers/index.html"
+  },
+  {
+    "revision": "cf6f2abdc6d0b01ebd7a",
+    "url": "/space-travelers/js/app.19e69161.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/space-travelers/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "78292d3e4b9cd184b7925f5371dde0ba",
+    "url": "/space-travelers/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/space-travelers/robots.txt"
+  },
+  {
+    "revision": "b660bac56ab777674f39c037b5530f45",
+    "url": "/space-travelers/screen-startup.png"
+  }
+]);
